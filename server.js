fea20e74f64c980950cb527e41f17fa3aace3f48
Kanishka998 <kanishkas998@gmail.com>
2016-09-23 01:05:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function (req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createTemplate(articleOne));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
