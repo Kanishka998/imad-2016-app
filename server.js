4458a67f573c256d1b1a11f867a90df77f0c98a9
Kanishka998 <kanishkas998@gmail.com>
2016-09-23 00:16:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+var content
+{
+    title:'Article one Kanishka Soni'.
+    heading:'Article-one'.
+    date:'sept 22 2016'.
+    content:
+};
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
